1fb431b7f14108eab85c8adb9718473f284616a4
Ahmad Raza <virtualahmadraza@gmail.com>
2023-03-28 10:17:15 +0500

    REACT-25-create command

---
+++ templates/default/react/config-overrides.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { processMainAppJs } = require("./config/salesforce");
+
+module.exports = function override(config, env) {
+    const isEnvDevelopment = env === 'development';
+    const isEnvProduction = env === 'production';
+    processMainAppJs();
+
+    //Custom work for salesforce
+    let fileName = isEnvProduction
+            ? 'static/js/main.bundle'+process.env.REACT_APP_BUNDLE_ID+'.js'
+            : isEnvDevelopment && 'static/js/main.bundle'+process.env.REACT_APP_BUNDLE_ID+'.js';
+    let chunkFilename = isEnvProduction
+            ? 'static/js/main.chunk'+process.env.REACT_APP_BUNDLE_ID+'.js'
+            : isEnvDevelopment && 'static/js/main.chunk'+process.env.REACT_APP_BUNDLE_ID+'.js';
+    let cssFilename = 'static/css/main'+process.env.REACT_APP_BUNDLE_ID+'.css';
+    let cssChunkFilename = 'static/css/main.chunk'+process.env.REACT_APP_BUNDLE_ID+'.css';
+    config.module.rules = [
+        // salesforce dependencies
+        // this will compile salesforce lightning as src, not as package
+        {
+            test: /\.jsx?$/,
+            include: [
+                'node_modules/@salesforce/design-system-react',
+            ].map(
+                someDir => path.resolve(
+                    process.cwd(),
+                    someDir
+                )
+            ),
+            loader: require.resolve('babel-loader'),
+            options: {
+                presets: [
+                    "react-app"
+                ],
+            },
+        },
+    ].concat(config.module.rules);
+
+    //Override js output file names
+    config.output = {
+        ...config.output,
+        // There will be one main bundle, and one file per asynchronous chunk.
+        // In development, it does not produce real files.
+        filename: fileName,
+        // There are also additional JS chunk files if you use code splitting.
+        chunkFilename: chunkFilename,
+        publicPath: process.env.REACT_APP_PUBLIC_URL
+    };
+
+    //Override css output file names
+    let plugins = [];
+    config.plugins.map((plugin, key) => {
+        if(plugin.hasOwnProperty("options") && plugin.options.hasOwnProperty("filename") && plugin.options.filename.indexOf("static/css/[name]") !== -1){
+            plugins[key] = new MiniCssExtractPlugin({
+                // Options similar to the same options in webpackOptions.output
+                // both options are optional
+                filename: cssFilename,
+                chunkFilename: cssChunkFilename,
+            });
+        }else {
+            plugins[key] = plugin;
+        }
+        return null;
+    });
+    config.plugins = plugins;
+    return config;
+}
